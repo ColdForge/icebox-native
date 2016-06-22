9c428b12c00dadd641cada39027873a81530b30c
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-21 18:51:04 -0700

    [pull] adds functionality for icebox rendering items with foodGroup icons in grid layout

--- src/components/iceboxList.js
+++ src/components/iceboxList.js
@@ -1,32 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
   ListView,
-  View,
+  ScrollView,
   TextInput,
   TouchableHighlight,
   Image
 } from 'react-native';
 import IceboxListItem from './iceboxListItem';
 
-const IceboxList = ({ contents }) => (
-	<View>
-		<ListView
-			dataSource={contents}
-			renderRow={(item) => (
-				<IceboxListItem
-					key={item.key}
-					item={item}
-					name={item.name}
-					foodGroup={item.foodGroup}
-					expiration={item.expiration}
-					iconPath={item.iconPath}
-				/>
-			)}
-		/>
-	</View>
-);
+const styles = StyleSheet.create({
+	gridList: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  gridListItem: {
+  	backgroundColor: '#CCC',
+    margin: 10,
+    width: 100,
+    height: 100
+  }
+})
+
+class IceboxList extends Component {
+	constructor(props){
+		super(props);
+		console.log('contents passed into IceboxList are : ',props.contents);
+		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		this.state = {
+			dataSource: ds.cloneWithRows(props.contents)
+		};
+	}
+
+	render(){
+		return (
+			<ListView
+				contentContainerStyle={styles.gridList}
+				dataSource={this.state.dataSource}
+				renderRow={(item) => (
+					<IceboxListItem
+						key={item.key}
+						item={item}
+						name={item.name}
+						foodGroup={item.foodGroup}
+						expiration={item.expiration}
+						iconPath={item.iconPath}
+					/>
+				)}
+			/>
+		);
+	}
+}
+
 
 IceboxList.propTypes = {
 	contents: React.PropTypes.array.isRequired,
--- src/components/iceboxListItem.js
+++ src/components/iceboxListItem.js
@@ -7,41 +7,77 @@ import {
   Image
 } from 'react-native';
 
-const styles = {
-	ListItem: {
-		color: '#000000'
+const styles = StyleSheet.create({
+	gridListItem: {
+		backgroundColor: '#CCC',
+	  margin: 10,
+	  width: 100,
+	  height: 100
 	},
-};
-
-const IceboxListItem = ({ name, foodGroup, iconPath, expiration }) => {
-	// if (!name) {
-	// 	return <ListItem primaryText="Loading..." />;
-	// }
-
-	let textColor = 'expiration-black';
-
-	if (expiration <= 3) {
-		textColor = 'expiration-red';
+	redItem: {
+		color: '#EA0B0B',
+		fontWeight: '700'
+	},
+	orangeItem: {
+		color: '#FF5A37',
+		fontWeight: '700'
+	},
+	normalItem: {
+		color: '#1FCA08',
+		fontWeight: '700'
+	},
+	fgIcon: {
+		width: 48,
+		height: 48
 	}
-	if (expiration > 3 && expiration <= 6) {
-		textColor = 'expiration-orange';
-	}
-	if (expiration > 7) {
-		textColor = 'expiration-black';
+});
+
+const iconsMap = { 
+	'fruit': require('../../assets/fruit.png'),
+	'vegetables': require('../../assets/vegetables.png'),
+	'dairy': require('../../assets/dairy.png'),
+	'meats': require('../../assets/meats.png'),
+	'poultry': require('../../assets/poultry.png'),
+	'pork': require('../../assets/pork.png'),
+	'beef': require('../../assets/beef.png'),
+	'seafood': require('../../assets/seafood.png'),
+	'grains': require('../../assets/grains.png'),
+	'sauces': require('../../assets/sauces.png'),
+	'legumes': require('../../assets/legumes.png'),
+	'sweets': require('../../assets/sweets.png'),
+	'snacks': require('../../assets/snacks.png'),
+	'oils': require('../../assets/oils.png'),
+	'beverages': require('../../assets/beverages.png'),
+	'lamb': require('../../assets/lamb.png'),
+	'nuts': require('../../assets/nuts.png'),
+	'sides': require('../../assets/sides.png'),
+	'default': require('../../assets/snacks.png')
+}
+
+const IceboxListItem = ({ styling, name, foodGroup, iconPath, expiration }) => {
+	foodGroup = foodGroup.toLowerCase();
+
+	const applyStyling = (expiration) => {
+		if (expiration <= 3) {
+			return styles.redItem;
+		}
+		if (expiration > 3 && expiration <= 6) {
+			return styles.orangeItem;
+		}
+		if (expiration >= 7) {
+			return styles.normalItem;
+		}
 	}
 
 	return (
-		<View
-			className="iceboxListItem"
-			style={styles.ListItem}
-		>
+		<View style={styles.gridListItem}>
 			<View>
-				<Image height="24" width="24" alt="Food Group" src={iconPath} />
+				<Image style={styles.fgIcon} source={iconsMap[foodGroup]} />
 				<View>
 					<Text>{name}</Text>
 				</View>
-				<View className={textColor}>
-					<Text>{expiration}</Text>
+				<View>
+					<Text style={applyStyling(expiration)}>{expiration} days</Text>
 				</View>
 			</View>
 		</View>
