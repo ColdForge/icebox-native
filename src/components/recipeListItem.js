7888c4d32cc64db7e22df047167b1820855e0780
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-22 12:46:54 -0700

    [pull] builds out recipeListItem component and improves styling

--- src/components/recipeListItem.js
+++ src/components/recipeListItem.js
@@ -4,31 +4,95 @@ import {
   Text,
   View,
   TouchableHighlight,
-  Image
+  Image,
+  Linking
 } from 'react-native';
 
 const styles = StyleSheet.create({
-	gridListItem: {
-		backgroundColor: '#CCC',
-	  margin: 10,
-	  width: 100,
-	  height: 100
+	row: {
+		height: 200,
+		backgroundColor: '#55C768',
+		flexDirection: 'row'
+	},
+	imageContainer: {
+		marginTop: 1,
+		marginBottom: 1,
+		flex: 1.5,
+		justifyContent: 'center',
+		alignItems: 'center',
+		position: 'relative'
+	},
+	image: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		bottom: 0,
+		right: 0
+	},
+	body: {
+		flex: 2,
+		paddingLeft: 10,
+		paddingRight: 10,
+		flexDirection: 'column',
+		alignSelf: 'center'
+	},
+	title: {
+		textAlign: 'center',
+		fontSize: 18,
+		color: 'black'
+	},
+	text: {
+		textAlign: 'center',
+		fontSize: 14,
+		color: 'white'
+	},
+	linkButton: {
+		backgroundColor: '#EEFABC',
+		marginTop: 10,
+		paddingTop: 4,
+		paddingBottom: 4,
+		paddingLeft: 10,
+		paddingRight: 10,
+		borderRadius: 10,
+		alignSelf: 'center'
+	},
+	button: {
+		backgroundColor: '#F14A2A',
+		marginBottom: 10,
+		paddingTop: 4,
+		paddingBottom: 4,
+		paddingLeft: 10,
+		paddingRight: 10,
+		borderRadius: 10,
+		alignSelf: 'center'
+	},
+	buttonText: {
+		textAlign: 'center'
 	}
 });
 
-const RecipeListItem = ({ name, imageUrl, sourceUrl, recipeID, prepTime }) => (
-	<View style={styles.gridListItem}>
-		<View>
-			<Image source={{uri: imageUrl}} />
-			<View>
-				<Text>{name}</Text>
+const RecipeListItem = ({ name, imageUrl, sourceUrl, recipeID, prepTime }) => {
+	
+	return (
+		<View style={styles.row}>
+			<View style={styles.body}>
+				<Text style={styles.title}>{name}</Text>
+				<Text style={styles.text}>
+				  Prep Time: {prepTime} min
+				</Text>
+				<TouchableHighlight
+					style={styles.linkButton}
+					onPress={() => Linking.openURL(sourceUrl)}
+				>
+					<Text style={styles.buttonText}>Recipe Information</Text>
+				</TouchableHighlight>
 			</View>
-			<View>
-				<Text>Prep Time: ${prepTime}min, Recipe ID: ${recipeID}</Text>
+			<View style={styles.imageContainer}>
+				<Image source={{uri : imageUrl}} style={styles.image} />
 			</View>
 		</View>
-	</View>
-);
+	);
+};
 
 RecipeListItem.propTypes = {
 	name: React.PropTypes.string.isRequired,
