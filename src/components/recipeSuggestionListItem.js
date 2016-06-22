a17d6f4c7db50328a7b1dfde5ecaa983501bd9e1
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-22 12:11:18 -0700

    [pull] adds link to recipe information and improves styling of recipeSuggestionListItem component

--- src/components/recipeSuggestionListItem.js
+++ src/components/recipeSuggestionListItem.js
@@ -5,23 +5,30 @@ import {
   View,
   ScrollView,
   Image,
-  TouchableHighlight
+  TouchableHighlight,
+  Linking
 } from 'react-native';
 
 const styles = StyleSheet.create({
 	row: {
 		flex: 1,
-		paddingLeft: 20,
-		paddingRight: 20,
 		backgroundColor: '#55C768',
 		flexDirection: 'row'
 	},
-	image: {
+	imageContainer: {
+		marginTop: 1,
+		marginBottom: 1,
 		flex: 1,
-		height: 125,
-		width: 125,
-		borderRadius: 10,
-		alignSelf: 'center'
+		justifyContent: 'center',
+		alignItems: 'center',
+		position: 'relative'
+	},
+	image: {
+		position: 'absolute',
+		top: 0,
+		left: 0,
+		bottom: 0,
+		right: 0
 	},
 	body: {
 		flex: 2,
@@ -38,8 +45,18 @@ const styles = StyleSheet.create({
 		fontSize: 16,
 		color: 'white'
 	},
+	linkButton: {
+		backgroundColor: '#EEFABC',
+		marginTop: 10,
+		paddingTop: 4,
+		paddingBottom: 4,
+		paddingLeft: 10,
+		paddingRight: 10,
+		borderRadius: 10,
+		alignSelf: 'center'
+	},
 	button: {
-		backgroundColor: '#FFFFFF',
+		backgroundColor: '#F14A2A',
 		marginTop: 10,
 		paddingTop: 4,
 		paddingBottom: 4,
@@ -53,26 +70,37 @@ const styles = StyleSheet.create({
 	}
 })
 
-const RecipeSuggestionListItem = ({ image, likes, recipe, title, chooseRecipe }) => (
-	<View style={styles.row}>
-	  <Image source={{uri : image}} style={styles.image} />
-	  <View style={styles.body}>
-		  <Text style={styles.title}>{title}</Text>
-		  <Text style={styles.text}>
-		    # Missing Ingredients: {recipe.missedIngredientCount}
-		  </Text>
-		  <Text style={styles.text}>
-		    # Used Ingredients: {recipe.usedIngredientCount}
-		  </Text>
-		  <TouchableHighlight
-		  	style={styles.button}
-		  	onPress={chooseRecipe}
-		  >
-		  	<Text style={styles.buttonText}>Choose this recipe!</Text>
-		  </TouchableHighlight>
+const RecipeSuggestionListItem = ({ image, likes, recipe, title, chooseRecipe }) => {
+	const recipeUrl = image.replace('recipeImages/','').replace('.jpg','');
+	return (
+		<View style={styles.row}>
+			<View style={styles.imageContainer}>
+				<Image source={{uri : image}} style={styles.image} />
+			</View>
+		  <View style={styles.body}>
+			  <Text style={styles.title}>{title}</Text>
+			  <Text style={styles.text}>
+			    # Missing Ingredients: {recipe.missedIngredientCount}
+			  </Text>
+			  <Text style={styles.text}>
+			    # Used Ingredients: {recipe.usedIngredientCount}
+			  </Text>
+			  <TouchableHighlight
+			  	style={styles.linkButton}
+			  	onPress={() => Linking.openURL(recipeUrl)}
+			  >
+			  	<Text style={styles.buttonText}>Recipe Information</Text>
+			  </TouchableHighlight>
+			  <TouchableHighlight
+			  	style={styles.button}
+			  	onPress={chooseRecipe}
+			  >
+			  	<Text style={styles.buttonText}>Choose this recipe!</Text>
+			  </TouchableHighlight>
+			</View>
 		</View>
-	</View>
-);
+	);
+};
 
 RecipeSuggestionListItem.propTypes = {
 	recipe: React.PropTypes.object.isRequired,
